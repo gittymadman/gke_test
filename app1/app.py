09718e5d52b56d4bc8cd871d2cdb5230de89260f
Vedhava Krishna <vedhava.t6988@ltimindtree.com>
2024-07-30 14:49:52 +0530

    changes

--- app1/app.py
+++ app1/app.py
@@ -8,9 +8,7 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World App!'
 
-@app.route('/new')
-def new():
-    return 'New World'
+
 # Run the Flask application if this file is executed directly
 if __name__ == '__main__':
     app.run(debug=True, port=8080,host='0.0.0.0')
